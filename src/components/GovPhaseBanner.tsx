ec7ddc1b6b80e968ba8a603bea5b1ad1e5df7c29
emma-nicol <emma.nicol1@justice.gov.uk>
2026-06-09 18:23:46 +0100

    content colour padding changes

--- src/components/GovPhaseBanner.tsx
+++ src/components/GovPhaseBanner.tsx
@@ -12,12 +12,42 @@ const oldBanner = () => (
 export default function GovPhaseBanner() {
   return (
     <div className="govuk-!-padding-top-2 govuk-!-padding-bottom-8">
-      <div className="govuk-notification-banner govuk-!-margin-bottom-0" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
+      <div
+        className="govuk-notification-banner govuk-!-margin-bottom-0"
+        role="region"
+        aria-labelledby="govuk-notification-banner-title"
+        data-module="govuk-notification-banner"
+      >
+        <div className="govuk-notification-banner__header">
+          <h2
+            className="govuk-notification-banner__title"
+            id="govuk-notification-banner-title"
+          >
+            This is a prototype
+          </h2>
+        </div>
+
         <div className="govuk-notification-banner__content">
-          <p className="govuk-notification-banner__heading" id="govuk-notification-banner-title">This is a prototype</p>
-          <p className="govuk-notification-banner__text">It is not a live service. It is available Monday to Friday, 9am to 5pm, until 17th April 2026.</p>
-          <p className="govuk-notification-banner__text"><a className="govuk-notification-banner__link" target="_blank" rel="noopener noreferrer"
-              href="https://forms.office.com/pages/responsepage.aspx?id=KEeHxuZx_kGp4S6MNndq2BWuCYiathpHp2puk8YlVv9UN01JWkhETE9KQ1lSSjFLVlNHOTFWN0ZDOC4u&route=shorturl">Give feedback</a> to help us improve the search results.</p>
+          <p className="govuk-notification-banner__heading">
+            This is not a live service. It only includes a sample of design system content.
+          </p>
+
+          <p className="govuk-body">
+            Try a few searches, then{" "}
+            <a
+              className="govuk-notification-banner__link"
+              href="https://forms.office.com/pages/responsepage.aspx?id=KEeHxuZx_kGp4S6MNndq2BWuCYiathpHp2puk8YlVv9UN01JWkhETE9KQ1lSSjFLVlNHOTFWN0ZDOC4u&route=shorturl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              give feedback
+            </a>{" "}
+            to tell us whether this idea is useful and how we can improve the results.
+          </p>
+
+          <p className="govuk-body">
+            Available until 5pm Friday 12 June 2026.
+          </p>
         </div>
       </div>
     </div>
